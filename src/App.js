4fc97b3ae586f4946ac63751a65e2f4f419ca06d
dagmarawieczorek <dagmara.sylwia.w@gmail.com>
2018-10-27 14:37:03 +0200

    handled picking color and changing color of stitch on click

--- src/App.js
+++ src/App.js
@@ -1,31 +1,77 @@
 import React, {Component} from 'react';
 import './App.css';
 import Box from "./components/Box.js";
-import { CompactPicker } from 'react-color';
+import {CompactPicker} from 'react-color';
 
 class App extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            pickedColor: "#B3B3B3"
+            pickedColor: "#B3B3B3",
+            width: 1,
+            height: 2,
+            stitchesWidth: [],
+            stitchesHeight: [],
         };
     }
 
+
+    handleChangeWidth = (event) => {
+        this.setState({width: Number(event.target.value)});
+    };
+
+    handleChangeHeight = (event) => {
+        this.setState({height: Number(event.target.value)});
+    };
+
+    handleChangeComplete = (color) => {
+        this.setState({pickedColor: color.hex});
+    };
+
+    generateChart = (e) => {
+        e.preventDefault();
+        for (let i = 1; i <= this.state.width; i++) {
+            this.state.stitchesWidth.push(i);
+        }
+
+        for (let i = 0; i < this.state.height; i++) {
+            this.state.stitchesHeight.push(this.state.stitchesWidth);
+        }
+        console.log(this.state.stitchesHeight);
+    }
+
+
     render() {
+        let BoxList = this.state.stitchesWidth.map((elem, index) =>
+            <Box color={this.state.pickedColor} key={index}/>
+        );
+
         return (
             <div className="App">
                 <h1>Type dimensions of knitting pattern</h1>
                 <div className="inputBox">
-                    <input type="number" className="input"/>
+                    <input type="number" className="input"
+                           value={this.state.width}
+                           onChange={this.handleChangeWidth}/>
+
                     <span>X</span>
-                    <input type="number" className="input"/>
-                    <button>Ok!</button>
+                    <input type="number" className="input"
+                           value={this.state.height}
+                           onChange={this.handleChangeHeight}/>
+                    <button
+                        onClick={this.generateChart}>Ok!
+                    </button>
 
                 </div>
-                <CompactPicker/>
+                <CompactPicker
+                    color={this.state.pickedColor}
+                    onChangeComplete={this.handleChangeComplete}/>
 
-                <Box color={this.state.pickedColor}/>
+
+                <div className="knittingBox">
+                    {BoxList}
+                </div>
             </div>
         );
     }
--- src/components/Box.js
+++ src/components/Box.js
@@ -1,12 +1,30 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class Box extends Component {
 
 
-    
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            bgColor: "#B3B3B3",
+            defaultColor: "#B3B3B3",
+            clicked: false,
+            }
+    }
+
+    handleClickedBox=()=> {
+        this.setState({bgColor: this.props.color,
+        clicked:!this.state.clicked})
+    }
+
     render() {
+
+
         return (
-            <div className="box" style={{background:this.props.color}}>
+            <div className="box"
+                 style={{background: this.state.clicked?this.state.bgColor:this.state.defaultColor}}
+                 onClick={this.handleClickedBox}>
 
             </div>
         );
