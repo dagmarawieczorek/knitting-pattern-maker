29f8edd5c41c3f8e62aa9a32664c0136b1ae5ad3
dagmarawieczorek <dagmara.sylwia.w@gmail.com>
2018-10-27 16:00:14 +0200

    added some styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
 import Box from "./components/Box.js";
-import {CompactPicker} from 'react-color';
+import {CirclePicker} from 'react-color';
 
 class App extends Component {
     constructor(props) {
@@ -32,13 +32,14 @@ class App extends Component {
     generateChart = (e) => {
         e.preventDefault();
 
-            if (this.state.stitchesWidth.length>0){
-                    this.setState({
-                    stitchesWidth: [],
-                        stitchesHeight: [],
-                        width: 0,
-                        height: 0,
-                });}
+        if (this.state.stitchesWidth.length > 0) {
+            this.setState({
+                stitchesWidth: [],
+                stitchesHeight: [],
+                width: 0,
+                height: 0,
+            });
+        }
 
         for (let i = 1; i <= this.state.width; i++) {
             this.state.stitchesWidth.push(i);
@@ -59,34 +60,48 @@ class App extends Component {
             }
         );
 
-        let AllStitches=this.state.stitchesHeight.map((elem,index)=>{
+        let AllStitches = this.state.stitchesHeight.map((elem, index) => {
             return <div className="rows" key={index}> {Rows} </div>
         });
 
+
         return (
             <div className="App">
-                <h1>Type dimensions of knitting pattern</h1>
-                <div className="inputBox">
-                    <input type="number" className="input"
-                           value={this.state.width}
-                           onChange={this.handleChangeWidth}/>
+                <div className="container">
 
-                    <span>X</span>
-                    <input type="number" className="input"
-                           value={this.state.height}
-                           onChange={this.handleChangeHeight}/>
-                    <button
-                        onClick={this.generateChart}>{this.state.stitchesWidth.length>0?"Clear!":"Ok!"}
-                    </button>
+                    <div className="navBox">
+                        <h1 className="title">Knitting pattern generator</h1>
+                        <h2>Type dimensions of your pattern</h2>
+                        <div className="inputBox">
+                            <input type="number" className="input"
+                                   value={this.state.width}
+                                   onChange={this.handleChangeWidth}/>
+
+                            <span className="multiply">X</span>
+                            <input type="number" className="input"
+                                   value={this.state.height}
+                                   onChange={this.handleChangeHeight}/>
+                            <button
+                                className="button"
+                                onClick={this.generateChart}>{this.state.stitchesWidth.length > 0 ? "Clear!" : "Ok!"}
+                            </button>
+                        </div>
+
+                        <CirclePicker
+                            width="300px"
+                            circleSize={35}
+                            color={this.state.pickedColor}
+                            onChangeComplete={this.handleChangeComplete}/>
+
+                    </div>
+
+                    <div className="knittingBox">
+                        {AllStitches}
+
+                    </div>
 
                 </div>
-                <CompactPicker
-                    color={this.state.pickedColor}
-                    onChangeComplete={this.handleChangeComplete}/>
 
-                <div className="knittingBox">
-                    {AllStitches}
-                </div>
             </div>
         );
     }
